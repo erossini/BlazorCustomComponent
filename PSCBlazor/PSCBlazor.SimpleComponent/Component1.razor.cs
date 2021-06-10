6defeb055d375b2e7d1b8e7864341b90f4279799
Enrico Rossini <enrico.rossini.uk@live.com>
2021-06-10 21:30:23 +0100

    First upload

---
+++ PSCBlazor/PSCBlazor.SimpleComponent/Component1.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace PSCBlazor.SimpleComponent
+{
+	public partial class Component1
+	{
+		[Parameter] public string ParentName { get; set; }
+		[Parameter] public string Text { get; set; }
+		[Parameter] public EventCallback<string> TextChanged { get; set; }
+
+        [Inject] private IJSRuntime JsRuntime { get; set; }
+
+        private async Task GetTextAsync()
+        {
+            // Call JS interop.
+            var text = await new ExampleJsInterop(JsRuntime).Prompt("Enter some text:");
+
+            // Trigger the changed event.
+            await TextChanged.InvokeAsync(text);
+
+            // Set the property value.
+            Text = text;
+        }
+    }
+}
